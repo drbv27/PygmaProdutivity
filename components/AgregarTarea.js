c3c075f0deb2a6e8663164f202310605201f4ad6
Julian Calle <julian0986@gmail.com>
2022-05-27 16:53:47 -0500

    creacion de formulacio de tareas por componentes

--- components/AgregarTarea.js
+++ components/AgregarTarea.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  ContGen,
+  ContProces,
+  ContProcesT,
+  ContTime,
+  InicioTitle,
+  Inputinicio,
+  InputProces,
+  InputTitle,
+  SectProces,
+  TextArea,
+  TitleSub,
+} from "./layout/FormTareas";
 
 const AgregarTarea = () => {
   return (
-    <div>AgregarTarea</div>
-  )
-}
+    <div>
+      <InicioTitle>Registrar Actividad</InicioTitle>
+      <ContGen>
+        <ContTime>
+          <InputTitle>Fecha</InputTitle>
+          <Inputinicio type="date" />
+        </ContTime>
+        <ContTime>
+          <InputTitle>Hora Inicio</InputTitle>
+          <Inputinicio type="time" />
+        </ContTime>
+        <ContTime>
+          <InputTitle>Hora Final</InputTitle>
+          <Inputinicio type="time" />
+        </ContTime>
+        <ContTime>
+          <InputTitle>Total horas</InputTitle>
+          <Inputinicio type="time" />
+        </ContTime>
+      </ContGen>
+      <SectProces>
+        <ContProces>
+          <TitleSub>Subproceso</TitleSub>
+          <InputProces type="list" />
+        </ContProces>
+        <ContProces>
+          <TitleSub>Proceso</TitleSub>
+          <InputProces type="text" />
+        </ContProces>
+        <ContProces>
+          <TitleSub>Macroproceso</TitleSub>
+          <InputProces type="text" />
+        </ContProces>
+      </SectProces>
+      <ContProcesT>
+        <TitleSub>Actividad realizada</TitleSub>
+        <TextArea cols="100" rows="5"></TextArea>
+      </ContProcesT>
+    </div>
+  );
+};
 
-export default AgregarTarea
+export default AgregarTarea;
---
+++ components/layout/FormTareas.js
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+
+export const InicioTitle = styled.p`
+  font-size: 2rem;
+  text-align: center;
+`;
+
+export const ContGen = styled.div`
+  display: flex;
+  gap: 100px;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+`;
+export const ContTime = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputTitle = styled.label`
+  font-size: 1.1rem;
+  text-align: center;
+  font-weight: 400;
+  margin-bottom: 5px;
+`;
+export const Inputinicio = styled.input`
+  width: 135px;
+  height: 40px;
+  border-radius: 10px;
+  font-size: 1rem;
+  border: 1px solid #1097d5;
+`;
+export const SectProces = styled.div`
+  display: flex;
+  gap: 100px;
+
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  margin-top: 30px;
+`;
+
+export const ContProces = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const TitleSub = styled.label`
+  font-size: 1.1rem;
+  font-weight: 400;
+  margin-left: 5px;
+  padding-right: 5px;
+`;
+export const InputProces = styled.input`
+  width: 165px;
+  height: 40px;
+  border-radius: 10px;
+  font-size: 1rem;
+  border: 1px solid #1097d5;
+`;
+
+export const TextArea = styled.textarea`
+  border: 1px solid #1097d5;
+`;
+export const ContProcesT = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 25px;
+  justify-content: center;
+`;
